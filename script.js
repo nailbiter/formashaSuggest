bc7dbd43a336058e7b003132c1145d98592b3eb1
レオンチエフ　アレックス <aloz1991@gmail.com>
2018-04-06 15:49:43 +0900

    first working

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function startSuggest() {
   new Suggest.Local(
         "text",    // input element id.
         "suggest", // suggestion area id.
-        ['Java', 'JavaScript' and 'perl', 'Ruby', 'PHP', 'Python', 'C' and 'C++', '. NET', 'MySQL', 'Oracle', 'PostgreSQL'],      // suggest candidates list
+        ['Java', 'JavaScript','perl', 'Ruby', 'PHP', 'Python', 'C',
+            'C++', '. NET', 'MySQL', 'Oracle', 'PostgreSQL'],      // suggest candidates list
         {dispMax: 10, interval: 1000}); // options
 }
 
