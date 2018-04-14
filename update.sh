5a52368ab14f195ee3d16d922eaac8e58af28431
レオンチエフ　アレックス <aloz1991@gmail.com>
2018-04-14 22:51:00 +0900

    update

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 rm -f data.csv
 make data.csv
 git commit -a -m "update"
-ssh -i ~/.ssh/id_rsa inp9822058@alumni.cs.nctu.edu.tw cd ~/public_html/formashaSuggest && git pull
+ssh -i ~/.ssh/id_rsa inp9822058@alumni.cs.nctu.edu.tw cd ./public_html/formashaSuggest && git pull
