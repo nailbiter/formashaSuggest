3f704d6265b74e1a218a15a622738d09d4a83672
レオンチエフ　アレックス <aloz1991@gmail.com>
2018-04-30 19:05:15 +0900

    commit

--- update.sh
+++ update.sh
@@ -4,4 +4,6 @@ rm -f data.csv
 make data.csv
 git commit -a -m "update"
 git push
-ssh inp9822058@alumni.cs.nctu.edu.tw mashaUpdate.sh
+
+#ssh inp9822058@alumni.cs.nctu.edu.tw mashaUpdate.sh
+ssh inp9822058@alumni.cs.nctu.edu.tw /u/cs/98/9822058/bin/mashaUpdate.sh
